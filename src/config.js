c356e4049b20148ddc96dd738314a290c946e114
Melissaf M <melissam200604@gmail.com>
2025-04-21 18:47:15 -0500

    app panas coop

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI =  process.env.MONGODB_URI || "mongodb+srv://juand200528:panas123@cluster0.tm47b3w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const MONGODB_URI =  process.env.MONGODB_URI || "mongodb+srv://melissam200604:panas2025@cluster0.abndtzx.mongodb.net/panascOOp?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
